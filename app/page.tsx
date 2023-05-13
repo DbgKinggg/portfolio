0d508acad6dcb7eefce9f05c74a81a11374e5ca2
Samuel Chen <samuel.chen@detectorinspector.com.au>
2023-05-13 15:01:32 +0800

    feat: complete basic page structure

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <main className="grid min-h-screen overflow-hidden bg-black text-white md:grid-cols-3 pt-32 pb-20 md:pt-0 md:pb-6">
+      <main className="grid min-h-screen overflow-hidden bg-black text-white lg:grid-cols-3 pt-32 pb-20 md:pt-0 md:pb-6">
         <MainIntro />
         <MainPanel />
       </main>
@@ -35,59 +35,72 @@ function MainIntro() {
   )
 }
 
+function GradientBlob() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={200}
+      height={200}
+      fill="none"
+      className="w-auto md:h-full absolute right-0 md:left-0 top-2 -z-10"
+      preserveAspectRatio="xMidYMid slice"
+    >
+      <circle
+        cx={100}
+        cy={100}
+        r={50}
+        fill="#8E80FF"
+        filter="url(#a)"
+        style={{
+          mixBlendMode: "normal",
+        }}
+      />
+      <defs>
+        <filter
+          id="a"
+          width="150%"
+          height="150%"
+          x="-25%"
+          y="-25%"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feGaussianBlur result="fx_foregroundBlur" stdDeviation={24} />
+          <feTurbulence
+            baseFrequency={5}
+            numOctaves={6}
+            result="noise"
+            stitchTiles="stitch"
+            type="fractalNoise"
+          />
+          <feDisplacementMap
+            in="fx_foregroundBlur"
+            in2="noise"
+            result="displacement"
+            scale={50}
+            xChannelSelector="R"
+            yChannelSelector="G"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
+
 function MainPanel() {
   return (
-    <section className="relative mt-10 md:mt-0 z-10 mx-5 lg:col-span-2 lg:flex lg:items-center lg:justify-center">
-      <div className="grid gap-y-3 mt-4 lg:mt-0 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-4 relative">
+    <section className="relative mt-10 md:mt-20 lg:mt-32 md:mb-10 z-10 mx-5 lg:col-span-2 lg:flex lg:items-center lg:justify-center">
+      <div className="grid gap-y-3 mt-4 lg:mt-0 lg:grid-cols-6 lg:gap-x-4 lg:gap-y-4 relative">
+        <GradientBlob />
         <IntroCard />
         <TwitterCard />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={200}
-          height={200}
-          fill="none"
-          className="w-auto h-full absolute left-0 top-2 z-0"
-          preserveAspectRatio="xMidYMid slice"
-        >
-          <circle
-            cx={100}
-            cy={100}
-            r={50}
-            fill="#8E80FF"
-            filter="url(#a)"
-            style={{
-              mixBlendMode: "normal",
-            }}
-          />
-          <defs>
-            <filter
-              id="a"
-              width="150%"
-              height="150%"
-              x="-25%"
-              y="-25%"
-              colorInterpolationFilters="sRGB"
-              filterUnits="userSpaceOnUse"
-            >
-              <feGaussianBlur result="fx_foregroundBlur" stdDeviation={24} />
-              <feTurbulence
-                baseFrequency={5}
-                numOctaves={6}
-                result="noise"
-                stitchTiles="stitch"
-                type="fractalNoise"
-              />
-              <feDisplacementMap
-                in="fx_foregroundBlur"
-                in2="noise"
-                result="displacement"
-                scale={50}
-                xChannelSelector="R"
-                yChannelSelector="G"
-              />
-            </filter>
-          </defs>
-        </svg>
+        <GohanGoCard />
+        <GohanGoCardDemo />
+        <AscendantInfo />
+        <AscendantCard />
+        <RoboticaCard />
+        <SkillsCard />
+        <LanguagesCard />
       </div>
     </section>
   )
@@ -96,7 +109,7 @@ function MainPanel() {
 function IntroCard() {
   return (
     <div
-      className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 md:col-span-2 md:px-20 md:py-20 backdrop-blur-lg"
+      className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 lg:col-span-4 lg:px-20 lg:py-20 backdrop-blur-lg"
       style={{
         background: 'radial-gradient(71.86% 50% at 50% 0%,rgba(168,127,255,.04) 0%,rgba(168,127,255,0) 100%),rgba(4,1,21,.1)',
         transition: '.45s cubic-bezier(.6,.6,0,1) border-color'
@@ -117,7 +130,7 @@ function TwitterCard() {
     <Link
       href="https://twitter.com/DbgKinggg"
       target="_blank"
-      className="group flex h-auto w-full flex-col rounded-2xl border-2 border-gray-400 bg-[#8ecdf8] px-4 py-4 md:w-52"
+      className="group flex h-auto w-full flex-col rounded-2xl border-2 border-gray-400 bg-[#8ecdf8] px-4 py-4 lg:col-span-2"
     >
       <svg
         className="h-14 w-14 transition group-hover:scale-105"
@@ -135,4 +148,63 @@ function TwitterCard() {
       </label>
     </Link>
   )
+}
+
+function GohanGoCard() {
+  return (
+    <div className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 lg:col-span-3 lg:px-10 lg:py-10">
+      <h3 className="text-gray-50 text-3xl font-bold">GohanGo!!</h3>
+      <a className="text-gray-500" href='https://gohango.art/' target='_blank'>gohango.art</a>
+    </div>
+  );
+}
+
+function GohanGoCardDemo() {
+  return (
+    <div className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 lg:col-span-3 lg:px-10 lg:py-10">
+      <h3 className="text-gray-50 text-3xl font-bold">NFT Maker</h3>
+    </div>
+  );
+}
+
+function AscendantInfo() {
+  return (
+    <div className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 lg:col-span-2 lg:px-10 lg:py-10">
+      <h3 className="text-gray-50 text-3xl font-bold">Ascendant</h3>
+      <a className="text-gray-500" href='https://gohango.art/' target='_blank'>ascendant.fun</a>
+    </div>
+  );
+}
+
+function AscendantCard() {
+  return (
+    <div className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 lg:col-span-4 lg:px-10 lg:py-10">
+      <h3 className="text-gray-50 text-3xl font-bold">YoYoYo</h3>
+    </div>
+  );
+}
+
+function RoboticaCard() {
+  return (
+    <div className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 lg:col-span-6 lg:px-10 lg:py-10">
+      <h3 className="text-gray-50 text-3xl font-bold">Robotica</h3>
+      <a className="text-gray-500" href='https://gohango.art/' target='_blank'>robotica.coffee</a>
+    </div>
+  );
+}
+
+function SkillsCard() {
+  return (
+    <div className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 lg:col-span-4 lg:px-10 lg:py-10">
+      <h3 className="text-gray-50 text-3xl font-bold">Skills</h3>
+    </div>
+  );
+}
+
+function LanguagesCard() {
+  return (
+    <div className="w-full relative z-10 rounded-2xl border-8 border-white/10 px-10 py-10 lg:col-span-2 lg:px-10 lg:py-10">
+      <h3 className="text-gray-50 text-3xl font-bold">I speak</h3>
+    </div>
+  );
 }
