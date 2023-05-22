7b230c6993c058b2c6806fce72a650ce96072f7a
Samuel Chen <samuel.chen@detectorinspector.com.au>
2023-05-22 23:07:09 +0800

    feat: finish language card

--- app/(site)/components/cards/LanguagesCard.tsx
+++ app/(site)/components/cards/LanguagesCard.tsx
@@ -1,13 +1,14 @@
 function LanguagesCard() {
   return (
     <div
-      className="relative z-10 w-full rounded-3xl px-10 py-10 lg:col-span-2 lg:px-10 lg:py-10"
-      style={{
-        background:
-          'linear-gradient(to right, rgb(55, 65, 81), rgb(17, 24, 39), rgb(0, 0, 0))',
-      }}
+      className="relative z-10 w-full rounded-3xl px-10 py-10 lg:col-span-2 lg:px-6 lg:py-10 bg-gradient-to-tr from-[#dce35b] via-[#45b649] via-30% to-[#dce35b] overflow-hidden"
     >
-      <h3 className="text-3xl font-bold text-gray-50">I speak</h3>
+      <h3 className="text-3xl font-bold">I speak</h3>
+      <div className="mt-8 flex flex-col space-y-2">
+        <div className="text-3xl lg:text-2xl xl:text-3xl uppercase font-bold tracking-widest text-white/90">English</div>
+        <div className="text-3xl lg:text-2xl xl:text-3xl uppercase font-bold tracking-widest text-white/80">Cantonese</div>
+        <div className="text-3xl lg:text-2xl xl:text-3xl uppercase font-bold tracking-widest text-white/70">Mandarin</div>
+      </div>
     </div>
   )
 }
