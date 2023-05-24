9f1fb341c599005af70ef19e4952b0dae9257cb2
Samuel Chen <samuel.chen@detectorinspector.com.au>
2023-05-24 19:15:41 +0800

    feat: update open graph data

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,26 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const title = 'Samuel Chen - Full-Stack Developer';
+const description = 'This is Samuel Chen, a Full-Stack Developer based in Melbourne, Australia. I do a little bit of Frontend, a little bit of Backend and a little bit of web3 things!';
+const url = 'https://samuelchen.me/';
+
 export const metadata = {
-  title: 'Samuel Chen - Full-Stack Developer',
-  description:
-    'This is Samuel Chen, a Full-Stack Developer based in Melbourne, Australia. I do a little bit of Frontend, a little bit of Backend and a little bit of web3 things!',
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url,
+    type: 'website'
+  },
+  twitter: {
+    title,
+    description,
+    url,
+    domain: 'samuelchen.me',
+    card: 'summary_large_image'
+  }
 }
 
 export default function RootLayout({
